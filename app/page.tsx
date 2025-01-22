f2835dcd218df72707e614b915d7404edbb7840c
Frederik Nielsen <fni@lruddannelse.dk>
2025-01-22 11:01:32 +0100

    logic fixes

--- app/page.tsx
+++ app/page.tsx
@@ -6,9 +6,11 @@ import classNames from "classnames";
 const getNextSibling = ({
   element,
   selector,
+  includeEmpty,
 }: {
   element: HTMLElement | null;
   selector: string;
+  includeEmpty: boolean;
 }) => {
   if (!element) {
     return null;
@@ -20,11 +22,13 @@ const getNextSibling = ({
   // If the sibling matches our selector, use it
   // If not, jump to the next sibling and continue the loop
   while (sibling) {
-    if (
-      sibling.matches(selector) &&
-      sibling.querySelector("input")?.value !== ""
-    )
-      return sibling;
+    if (sibling.matches(selector)) {
+      if (includeEmpty) {
+        return sibling;
+      } else if (sibling.querySelector("input")?.value !== "") {
+        return sibling;
+      }
+    }
     sibling = sibling.nextElementSibling;
   }
   return sibling;
@@ -33,9 +37,11 @@ const getNextSibling = ({
 const getPreviousSibling = ({
   element,
   selector,
+  includeEmpty,
 }: {
   element: HTMLElement | null;
   selector: string;
+  includeEmpty: boolean;
 }) => {
   if (!element) {
     return null;
@@ -49,11 +55,13 @@ const getPreviousSibling = ({
   // If the sibling matches our selector, use it
   // If not, jump to the next sibling and continue the loop
   while (sibling) {
-    if (
-      sibling.matches(selector) &&
-      sibling.querySelector("input")?.value !== ""
-    )
-      return sibling;
+    if (sibling.matches(selector)) {
+      if (includeEmpty) {
+        return sibling;
+      } else if (sibling.querySelector("input")?.value !== "") {
+        return sibling;
+      }
+    }
     sibling = sibling.previousElementSibling;
   }
   return sibling;
@@ -62,19 +70,19 @@ const getPreviousSibling = ({
 export default function Home() {
   return (
     <p className="p-5 bg-white text-black">
-      <Word word="How" />
-      <Word word=" " />
-      <Word word="is" />
-      <Word word=" " />
-      <Word word="it" />
-      <Word word=" " />
-      <Word word="go ing" />
+      <Word word="How" id="1" />
+      <Word word=" " id="2" />
+      <Word word="is" id="3" />
+      <Word word=" " id="4" />
+      <Word word="it" id="5" />
+      <Word word=" " id="6" />
+      <Word word="go ing" id="7" />
       <br />
-      <Word word="This" />
-      <Word word=" " />
-      <Word word="is" />
-      <Word word=" " />
-      <Word word="Fun" />
+      <Word word="This" id="8" />
+      <Word word=" " id="9" />
+      <Word word="is" id="10" />
+      <Word word=" " id="11" />
+      <Word word="Fun" id="12" />
     </p>
   );
 }
@@ -82,34 +90,54 @@ export default function Home() {
 const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
   const target = e.target as HTMLInputElement;
   const element = target.closest(".word") as HTMLElement | null;
-  const prevWord = getPreviousSibling({ element, selector: ".word" });
-  const nextWord = getNextSibling({ element, selector: ".word" });
+
+  const prevWordIncludeEmpty = getPreviousSibling({
+    element,
+    selector: ".word",
+    includeEmpty: true,
+  });
+  const prevWordExcludeEmpty = getPreviousSibling({
+    element,
+    selector: ".word",
+    includeEmpty: false,
+  });
+
+  const nextWordIncludeEmpty = getNextSibling({
+    element,
+    selector: ".word",
+    includeEmpty: true,
+  });
+  const nextWordExcludeEmpty = getNextSibling({
+    element,
+    selector: ".word",
+    includeEmpty: false,
+  });
 
   const start = target.selectionStart || 0;
   const length = target.value.length;
 
   if (e.key === "Delete") {
-    if (start === length && nextWord) {
-      const input = nextWord.querySelector("input");
+    if (start === length && nextWordExcludeEmpty) {
+      const input = nextWordExcludeEmpty.querySelector("input");
       input?.focus();
       input?.setSelectionRange(0, 0);
     }
   } else if (e.key === "Backspace") {
-    if (start === 0 && prevWord) {
-      const input = prevWord.querySelector("input");
+    if (start === 0 && prevWordExcludeEmpty) {
+      const input = prevWordExcludeEmpty.querySelector("input");
       input?.focus();
       input?.setSelectionRange(input.value.length, input.value.length);
     }
   } else if (e.key === "ArrowRight") {
-    if (start === length && nextWord) {
-      const input = nextWord.querySelector("input");
+    if (start === length && nextWordExcludeEmpty) {
+      const input = nextWordExcludeEmpty.querySelector("input");
       input?.focus();
       input?.setSelectionRange(1, 1);
       e.preventDefault();
     }
   } else if (e.key === "ArrowLeft") {
-    if (start === 0 && prevWord) {
-      const input = prevWord.querySelector("input");
+    if (start === 0 && prevWordExcludeEmpty) {
+      const input = prevWordExcludeEmpty.querySelector("input");
       input?.focus();
       input?.setSelectionRange(input.value.length - 1, input.value.length - 1);
     }
@@ -128,8 +156,9 @@ const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
       if (
         prevChar === " " ||
         nextChar === " " ||
-        (prevWord === null && target.selectionStart === 0) ||
-        (nextWord === null && target.selectionStart === target.value.length)
+        (prevWordIncludeEmpty === null && target.selectionStart === 0) ||
+        (nextWordIncludeEmpty === null &&
+          target.selectionStart === target.value.length)
       ) {
         e.preventDefault();
       } else {
@@ -137,8 +166,11 @@ const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
           prevChar === null &&
           element?.querySelector(".original span")?.innerHTML !== " "
         ) {
-          if (prevWord?.querySelector(".original span")?.innerHTML === " ") {
-            const prevInput = prevWord?.querySelector("input");
+          if (
+            prevWordIncludeEmpty?.querySelector(".original span")?.innerHTML ===
+            " "
+          ) {
+            const prevInput = prevWordIncludeEmpty?.querySelector("input");
             if (prevInput?.value === "") {
               prevInput?.focus();
             } else {
@@ -149,40 +181,49 @@ const onKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
           nextChar === null &&
           element?.querySelector(".original span")?.innerHTML !== " "
         ) {
-          if (nextWord?.querySelector(".original span")?.innerHTML === " ") {
-            const nextInput = nextWord?.querySelector("input");
+          if (
+            nextWordIncludeEmpty?.querySelector(".original span")?.innerHTML ===
+            " "
+          ) {
+            const nextInput = nextWordIncludeEmpty?.querySelector("input");
             if (nextInput?.value === "") {
               nextInput?.focus();
             } else {
               e.preventDefault();
             }
-            //
           }
         }
       }
     } else if (
       prevChar === " " &&
-      nextWord &&
+      nextWordExcludeEmpty &&
       target.selectionStart !== null &&
       target.selectionStart - 1 === 0
     ) {
-      const input = nextWord.querySelector("input");
+      const input = nextWordExcludeEmpty.querySelector("input");
       input?.focus();
       input?.setSelectionRange(0, 0);
     } else if (
       nextChar === " " &&
-      prevWord &&
+      prevWordExcludeEmpty &&
       target.selectionStart !== null &&
       target.selectionStart + 1 === target.value.length
     ) {
-      const input = prevWord.querySelector("input");
+      const input = prevWordExcludeEmpty.querySelector("input");
       input?.focus();
       input?.setSelectionRange(input.value.length, input.value.length);
     }
   }
 };
 
-const Word = ({ word }: { word: string }) => {
+const Word = ({ word, id }: { word: string; id: string }) => {
+  const split = word.split(/(\s)/g).filter((item) => item.length);
+  return split.map((elem, i) => {
+    return <SplitWord key={i} word={elem} id={id} />;
+  });
+};
+
+const SplitWord = ({ word, id }: { word: string; id: string }) => {
   const ref = useRef<HTMLSpanElement>(null);
   const [value, setValue] = useState<string>(word);
 
@@ -191,10 +232,12 @@ const Word = ({ word }: { word: string }) => {
       const prevWord = getPreviousSibling({
         element: ref.current,
         selector: ".word",
+        includeEmpty: false,
       })?.querySelector(".original");
       const nextWord = getNextSibling({
         element: ref.current,
         selector: ".word",
+        includeEmpty: false,
       })?.querySelector(".original");
       const input = ref.current?.querySelector("input");
       if (value === "") {
@@ -224,6 +267,7 @@ const Word = ({ word }: { word: string }) => {
   return (
     <span
       className="word relative inline-flex items-end flex-col h-16"
+      data-id={id}
       ref={ref}
     >
       <span
@@ -243,7 +287,7 @@ const Word = ({ word }: { word: string }) => {
       </span>
       <input
         type="text"
-        className="absolute bottom-0 left-0 right-0 border-none focus:outline-none text-inherit h-6"
+        className="focus:bg-gray-300 absolute bottom-0 left-0 right-0 border-none focus:outline-none text-inherit h-6"
         onChange={(e) => setValue(e.target.value)}
         onKeyDown={onKeyDown}
         value={value}
